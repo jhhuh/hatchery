83e3ec398984dcf448fd69736a0e38de279b5a24
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-15 12:45:30 +0900

    fix: use haskellPackages.shellFor with llvm-ffi/llvm-tf 21 + LLVM 19
    - Switch flake to haskellPackages.shellFor for proper Haskell dev shell
    - Override haskellPackages with callHackage for llvm-ffi 21.0.0.2 + llvm-tf 21.0
    - Use LLVM 19 (llvm-ffi 21 needs LLVMGEPNoWrapFlags from LLVM 19+)
    - All three packages now build: hatchery, hatchery-llvm, trustless-ffi

--- flake.nix
+++ flake.nix
@@ -10,21 +10,31 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
       musl-cc = pkgs.pkgsStatic.stdenv.cc;
-    in {
-      devShells.${system}.default = pkgs.mkShell {
+
+      # Override haskellPackages with pinned llvm-ffi/llvm-tf versions
+      hsPkgs = pkgs.haskellPackages.override {
+        overrides = hself: hsuper: {
+          llvm-ffi = hself.callHackage "llvm-ffi" "21.0.0.2" {};
+          llvm-tf  = hself.callHackage "llvm-tf"  "21.0" {};
+        };
+      };
+
+      # Dev shell with all Haskell deps pre-built
+      haskellShell = hsPkgs.shellFor {
+        packages = _: [];  # local packages built by cabal, not nix
         buildInputs = [
-          pkgs.ghc
           pkgs.cabal-install
           musl-cc
-          pkgs.llvmPackages_18.llvm
+          pkgs.llvmPackages_19.llvm
           pkgs.nasm
           pkgs.overmind
           pkgs.tmux
         ];
-
         shellHook = ''
           export MUSL_CC="${musl-cc}/bin/x86_64-unknown-linux-musl-gcc"
         '';
       };
+    in {
+      devShells.${system}.default = haskellShell;
     };
 }
